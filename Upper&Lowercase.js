f4dc78b42e75eb00d1d06ab4e9517808e5e51e5f
abhijitWa <140107345+abhijitWa@users.noreply.github.com>
2024-09-20 12:07:06 +0530

    Add files via upload

---
+++ Upper&Lowercase.js
@@ -0,0 +1,12 @@
+
+let greeting = "HeLLo WoRlD";
+
+
+let upperCaseGreeting = greeting.toUpperCase();
+
+
+console.log(upperCaseGreeting); 
+
+let LowerCaseGreeting = greeting.toLowerCase();
+
+console.log(LowerCaseGreeting); 
